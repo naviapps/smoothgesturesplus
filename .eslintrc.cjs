a74191dbe5b54d133b99fcd0184c43ef898a7781
Haruki Fukui <haruki.fukui@naviapps.com>
2024-04-14 17:00:41 +0900

    run prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  ignorePatterns: ['.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+}
