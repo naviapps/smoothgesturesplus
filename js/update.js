44616470c361ec5102c854e8c3544abe9cde117b
Haruki Fukui <haruki.fukui@naviapps.com>
2025-02-23 16:56:13 +0900

    add 2.9

---
+++ js/update.js
@@ -0,0 +1,1 @@
+$(function(){"update_url"in chrome.runtime.getManifest()&&(console.log=console.error=function(){}),$("#openextensions").click(function(e){chrome.tabs.create({url:"chrome://extensions/"}),e.preventDefault()})});
